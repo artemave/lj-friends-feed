163b28fc59daff06ac2f28a8ba8b024ea9245066
artemave <artemave@gmail.com>
2012-06-25 15:03:44 +0100

    introduce test failure so that I know where to pick it up

--- spec/matchers/feed_matchers.rb
+++ spec/matchers/feed_matchers.rb
@@ -1,5 +1,6 @@
 RSpec::Matchers.define :have_data_from do |internal_feed|
   match do |actual_feed|
     actual_feed.channel.title =~ /#{internal_feed.username}/
+    actual_feed.items.size.should == internal_feed.items.size
   end
 end
