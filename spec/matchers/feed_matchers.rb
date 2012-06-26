a4159205e781d750c8a36c59a857c2be3955c75f
artemave <artemave@gmail.com>
2012-06-26 14:47:26 +0100

    better failure reporting when comparing feeds

--- spec/matchers/feed_matchers.rb
+++ spec/matchers/feed_matchers.rb
@@ -1,6 +1,19 @@
 RSpec::Matchers.define :have_data_from do |internal_feed|
   match do |actual_feed|
-    actual_feed.channel.title =~ /#{internal_feed.username}/
-    actual_feed.items.size.should == internal_feed.entries.size
+    @errors = []
+    
+    if actual_feed.channel.title !~ /#{internal_feed.username}/
+      @errors << "Feed title mismatch: expected: '#{actual_feed.channel.title}' to contain '#{internal_feed.username}'"
+    end
+
+    if actual_feed.items.size != internal_feed.entries.size
+      @errors << "Number of feed entries don't match: expected: '#{internal_feed.entries.size}' got: '#{actual_feed.items.size}'"
+    end
+
+    @errors.empty?
+  end
+
+  failure_message_for_should do
+    @errors.join("\n")
   end
 end
