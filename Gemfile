616b15f543f4b0ed8706b6035fc3785eeb012dda
artemave <artemave@gmail.com>
2012-06-26 14:03:39 +0100

    add factory_girl and faker

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ source :rubygems
 group :test do
   gem 'cucumber', require: false
   gem 'rspec'
+  gem 'factory_girl'
+  gem 'faker'
   gem 'awesome_print'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,11 @@
 Bundler.require :default, :test
+
 require 'rss'
+require 'factory_girl'
 require_relative '../../spec/matchers/feed_matchers'
 
 Dir[File.expand_path '../../../lib/*.rb', __FILE__].each &method(:require)
+
+World(FactoryGirl::Syntax::Methods)
+
+FactoryGirl.find_definitions
