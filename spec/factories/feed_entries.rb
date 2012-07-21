a3b1fb66ac36301082f83682cce4263bc88fd54b
artemave <artemave@gmail.com>
2012-07-21 18:46:59 +0100

    put back accidently removed link field from entry factory

--- spec/factories/feed_entries.rb
+++ spec/factories/feed_entries.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
   factory :entry do
     author { Faker::Name.first_name.downcase }
     title { Faker::Lorem.sentence }
+    link "http://myexample.com/post/link"
     content { Faker::Lorem.paragraph }
     pub_date { Time.parse "Mon, 14 May 2012 21:#{generate :counter}:16 GMT" }
   end
