9058bda8e282a4c562b698a5ef7963f2e2d5029b
artemave <artemave@gmail.com>
2014-09-18 17:18:36 +0100

    new rspec syntax

--- spec/update_feed_task_spec.rb
+++ spec/update_feed_task_spec.rb
@@ -6,23 +6,23 @@ describe UpdateFeedTask do
   let(:feed_updater) { double(:feed_updater) }
   let(:feed_persistence_service) { double(:feed_persistence_service) }
   let(:update_feed_task) { UpdateFeedTask.new feed_persistence_service, feed_updater }
+  let(:feed1) { Feed.new username: 'alice' }
+  let(:feed2) { Feed.new username: 'bob' }
+
+  before do
+    allow(feed_persistence_service).to receive(:all).and_return([feed1, feed2])
+  end
 
   it 'updates all existing feeds' do
-    feed1, feed2 = Feed.new(username: 'alice'), Feed.new(username: 'bob')
-    feed_persistence_service.stub(:all).and_return([feed1, feed2])
-
-    feed_updater.should_receive(:update).with(feed1)
-    feed_updater.should_receive(:update).with(feed2)
+    expect(feed_updater).to receive(:update).with(feed1)
+    expect(feed_updater).to receive(:update).with(feed2)
 
     update_feed_task.invoke
   end
 
   it 'isolates individual update feed failure' do
-    feed1, feed2 = Feed.new(username: 'alice'), Feed.new(username: 'bob')
-    feed_persistence_service.stub(:all).and_return([feed1, feed2])
-
-    feed_updater.should_receive(:update).with(feed1).and_raise
-    feed_updater.should_receive(:update).with(feed2).and_raise
+    expect(feed_updater).to receive(:update).with(feed1).and_raise
+    expect(feed_updater).to receive(:update).with(feed2).and_raise
 
     update_feed_task.invoke
   end
